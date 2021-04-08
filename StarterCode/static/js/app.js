164b1bdf248e437f90a58b8ce990632b9cf091bc
Ciera Morris <cieranmorris@gmail.com>
2021-04-07 23:07:21 -0600

    initiated forEach connection of data.js to table via app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,16 @@
+console.log("app.js is loaded");
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(data);
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Iterate through array of objects
+data.forEach(function(ufoSightings) {
+
+    console.log(ufoSightings);
+    var row = tbody.append("tr");
+});
