946af97cbe6d0abbba48c016be18527736d9158d
Ciera Morris <cieranmorris@gmail.com>
2021-04-07 23:19:01 -0600

    appended key-value pairs to table body

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,9 +8,18 @@ console.log(data);
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Iterate through array of objects
+// Iterate through array of objects and append a table row to each element in the array
 data.forEach(function(ufoSightings) {
 
     console.log(ufoSightings);
     var row = tbody.append("tr");
+
+    // take key-value pairs and append to table columns
+    Object.entries(ufoSightings).forEach(function([key, value]) {
+        console.log("Combing through new key-value pair:");
+        console.log(key, value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
 });
